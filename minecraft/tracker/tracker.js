9f5e4b1c22f14be6ed2609536bc78a3b691e5d0b
Hypersun_pro <51555391+hypersun-pro@users.noreply.github.com>
2020-09-04 17:13:53 +0800

    Minecraft Player Tracker 1.0 Update

--- minecraft/tracker/tracker.js
+++ minecraft/tracker/tracker.js
@@ -12,10 +12,11 @@ function trackerStart(){
         }
     }
     var request = new XMLHttpRequest();
+    var firstRequest = new XMLHttpRequest();
     var tracker = {
         option : {
             user: document.getElementById('trackerInputFormsUser').value,
-            uuid: '',
+            uuid: document.getElementById('trackerInputFormsUser').value,
             name: '',
             interval: 10,
             hypixelKey: 'eb954b0a-acc4-4d97-983e-4758f3eaa410',
@@ -55,10 +56,12 @@ function trackerStart(){
         mem: {
             online: false,
             game: '',
+            lastLogout: new Date(),
         },
         consoleContent: ''
     };
     tracker.docLog('Support <a href="https://www.youtube.com/channel/UC-FLvOdIGGLOIRvKT9_QtOg" target="_blank">Hypersun_pro</a> on Youtube');
+    tracker.docLog('Leave this page AFK, and I will report to you when someone go online');
     if (document.getElementById('trackerInputFormsInterval') && Number(document.getElementById('trackerInputFormsInterval').value)&& document.getElementById('trackerInputFormsInterval').value != ""){
         if (document.getElementById('trackerInputFormsInterval').value < 5){
             tracker.docLog('Due to network safety, your interval value is too low. Interval value will automatically set to 5');
@@ -69,70 +72,58 @@ function trackerStart(){
     }
     if (document.getElementById('trackerInputFormsHypixelKey') && document.getElementById('trackerInputFormsHypixelKey').value != ""){tracker.option.hypixelKey = document.getElementById('trackerInputFormsHypixelKey').value;}
     
-    
-    if (tracker.option.user.length < 17){
-        request.open('get', `https://api.mojang.com/users/profiles/minecraft/${tracker.option.user}`);
-        request.onload = function(){
-            var respond = JSON.parse(request.response);
-            if(respond.id && respond.name){
-                tracker.option.uuid = respond.id;
-                tracker.option.name = respond.name;
-                tracker.docLog(`Success: start tracking player: ${tracker.option.name} (${tracker.option.uuid})`);
-            }
-        };
-        request.send();
-    } else {
-        tracker.option.user = tracker.option.user.replace("-", "");
-        request.open('get', `https://api.mojang.com/user/profiles/${tracker.option.user}/names`);
-        request.onload = function(){
-            var respond = JSON.parse(request.response);
-            if(!respond.error){
-                var length = respond.length - 1;
-                tracker.option.uuid = tracker.option.user;
-                tracker.option.name = respond[length].name;
-                tracker.docLog(`Success: start tracking player: ${tracker.option.name} (${tracker.option.uuid})`);
-            }
-        };
-        request.send();
-    }
-    
-    if(!tracker.option.uuid || !tracker.option.name) return tracker.docLog('Error: invalid uuid or name');
-
-    request.open('get', `https://api.hypixel.net/status?key=${tracker.option.hypixelKey}&uuid=${tracker.option.uuid}`);
-    request.onloadend = function() {
-        var respond = JSON.parse(request.response);
-        if (!respond.success){
-            tracker.docLog(`Error from Hypixel: ${respond.cause}`);
+    //check uuid and name on hypixel
+    firstRequest.open('get', `https://api.hypixel.net/player?key=${tracker.option.hypixelKey}&uuid=${tracker.option.uuid}`);
+    firstRequest.onloadend = function(){
+        var respond = JSON.parse(firstRequest.response);
+        if (!respond.success) {
+            tracker.docLog(`Error from Hypixel: ${respond.reason}`);
+            return;
         } else {
-            if (respond.session.online){
-                var nowtime = new Date();
-                tracker.docLog(`Player now online, game = ${respond.session.game} (${new Date().toLocaleTimeString()})`);
-                notify('now online');
-                tracker.mem.online = true;
-                tracker.mem.game = respond.session.game;
-            } else {
-                request.open('get', `https://api.hypixel.net/player?key=${tracker.option.hypixelKey}&uuid=${tracker.option.uuid}`);
-                request.onloadend = function() {
-                    var respond = JSON.parse(request.response);
-                    var time = new Date(respond.player.lastLogin);
-                    var lastLogin = time.toLocaleDateString() + ' ' + time.toLocaleTimeString();
-                    tracker.docLog(`Player offline (Last online: ${lastLogin})`);
-                };
-                request.send();
-            }
+            tracker.option.name = respond.player.playername;
+            tracker.mem.lastLogout = new Date(respond.player.lastLogout);
+            tracker.docLog(`Successfully connect to Hypixel`);
+            tracker.docLog(`Start tracking ${tracker.option.name}`);
+            firstCheck();
         }
     };
-    request.send();
-    request.timeout = 100000;
-    request.ontimeout = function(){
-        tracker.docLog('Error: connection timeout(reply from hypixel was too slow)');
-    };
+    firstRequest.send();
+    firstRequest.timeout = 10000;
+
+    //get first session check
+    function firstCheck(){
+        request.open('get', `https://api.hypixel.net/status?key=${tracker.option.hypixelKey}&uuid=${tracker.option.uuid}`);
+        request.onloadend = function() {
+            var respond = JSON.parse(request.response);
+            if (!respond.success){
+                tracker.docLog(`Error from Hypixel: ${respond.cause}`);
+                return;
+            } else {
+                if (respond.session.online){
+                    var nowtime = new Date();
+                    tracker.docLog(`Player now online, game: ${respond.session.gameType} (${new Date().toLocaleTimeString()})`);
+                    notify('now online');
+                    tracker.mem.online = true;
+                    tracker.mem.game = respond.session.gameType;
+                } else {
+                    tracker.docLog(`Player offline (Last online: ${tracker.mem.lastLogout.toLocaleDateString()} ${tracker.mem.lastLogout.toLocaleTimeString()})`);
+                }
+            }
+        };
+        request.send();
+        request.timeout = 100000;
+        request.ontimeout = function(){
+            tracker.docLog('Error: connection timeout(internet too slow)');
+        };
+    }
+    //real check
     function checkHypixel(){
         request.open('get', `https://api.hypixel.net/status?key=${tracker.option.hypixelKey}&uuid=${tracker.option.uuid}`);
         request.onloadend = function() {
             var respond = JSON.parse(request.response);
             if (!respond.success){
                 tracker.docLog(`Error from Hypixel: ${respond.cause}`);
+                return;
             } else {
                 if (respond.session.online){
                     if (!tracker.mem.online){
@@ -144,15 +135,14 @@ function trackerStart(){
                         tracker.mem.game = respond.session.game;
                     } else {
                         tracker.calculation.addtime();
-                        if (respond.session.game != tracker.mem.game){
-                            tracker.mem.game = respond.session.game;
-                            tracker.docLog(`Player change game to ${tracker.mem.game} (${new Date().toLocaleTimeString()})`);
+                        if (respond.session.gameType != tracker.mem.game){
+                            tracker.mem.game = respond.session.gameType;
+                            tracker.docLog(`Player change game to ${tracker.mem.gameType} (${new Date().toLocaleTimeString()})`);
                             notify('change game');
                         }
                     }
                 } else {
                     if (tracker.mem.online){
-                        var nowtime = new Date();
                         tracker.docLog(`Player now offline (${new Date().toLocaleTimeString()})`);
                         notify('now online');
                         tracker.mem.online = false;
