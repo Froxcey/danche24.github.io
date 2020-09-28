3178937191c44de060cfb0530e044f801a8398b0
Hypersun_pro <51555391+hypersun-pro@users.noreply.github.com>
2020-09-28 11:21:10 +0800

    broadcast admin and broadcast clean up

---
+++ broadcast/broadcast.js
@@ -0,0 +1,24 @@
+import firebase from "firebase";
+var firebaseConfig = {
+    apiKey: "AIzaSyAcrcquKzNjKh1PByVK4NglsVlnYXyeDJU",
+    authDomain: "application-service-a1b32.firebaseapp.com",
+    databaseURL: "https://application-service-a1b32.firebaseio.com",
+    projectId: "application-service-a1b32",
+    storageBucket: "application-service-a1b32.appspot.com",
+    messagingSenderId: "164827554782",
+    appId: "1:164827554782:web:b4b1a42c1d8acc753d334f",
+    measurementId: "G-80953CEHTZ"
+};
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+var databaseBroadcast = firebase.database().ref().child('broadcast')
+databaseBroadcast.on('value', snap => {
+    var broadcastSound = new Audio()
+    broadcastSound.src = `data:audio/mp3;base64,${snap.val()}`
+    var initSound = new Audio()
+    initSound.src = 'http://danche24.github.io/broadcast/intro.mp3'
+    initSound.play()
+    setInterval(function(){
+        broadcastSound.play()
+    }, 3000)
+})
